5fcd6554a8c5857ac6d1edd3382fb2742f73ec8f
Artem Rakov <scoff1k@gmail.com>
2019-12-22 20:57:07 +0300

    changed class component to react.hooks

--- app/javascript/src/containers/NewMessage.js
+++ app/javascript/src/containers/NewMessage.js
@@ -1,50 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 
-class NewMessage extends React.Component {
-  state = {
-    content: ''
+const newMessage = (props) => {
+  const [content, setContent] = useState('');
+
+  const handleChange = (event) => {
+    setContent(event.target.value);
   }
 
-  handleChange = (event) => {
-    this.setState({ content: event.target.value })
-  }
-
-  handleSubmit = async (event) => {
+  const handleSubmit = async (event) => {
     event.preventDefault();
 
-    const response = await axios.post(`/channels/${this.props.channel.id}/messages`, {
-      team_channel_message: { content: this.state.content }
+    const response = await axios.post(`/channels/${props.channel.id}/messages`, {
+      team_channel_message: { content }
     })
 
-    this.setState({ content: '' });
+    setContent('');
   }
 
-  render() {
-    if (this.props.allowed) {
-      return (
-        <form onSubmit={this.handleSubmit}>
-          <div className="form-group string">
-            <input onChange={this.handleChange} value={this.state.content} className="form-control string" name="team_channel_message[content]" />
-          </div>
+  if (props.allowed) {
+    return (
+      <form onSubmit={handleSubmit}>
+        <div className="form-group string">
+          <input onChange={handleChange} value={content} className="form-control string" name="team_channel_message[content]" />
+        </div>
+        <button className="btn btn-primary">
+          Sent
+        </button>
+      </form>
+    );
+  } else {
+    return (
+      <span className='text-center'>
+        In order to talk, you need to join team
+        <form action={`/teams/${props.team.id}/join`} method="post">
           <button className="btn btn-primary">
-            Sent
+            Join team
           </button>
         </form>
-      );
-    } else {
-      return (
-        <span className='text-center'>
-          In order to talk, you need to join team
-          <form action={`/teams/${this.props.team.id}/join`} method="post">
-            <button className="btn btn-primary">
-              Join team
-            </button>
-          </form>
-        </span>
-      )
-    }
+      </span>
+    )
   }
 };
 
-export default NewMessage;
+export default newMessage;
