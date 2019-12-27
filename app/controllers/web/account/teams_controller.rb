cddd59e4c82c8c9baa33e188ef71ee3bb599a21d
Artem Rakov <scoff1k@gmail.com>
2019-12-27 13:58:52 +0300

    added flashes

--- app/controllers/web/account/teams_controller.rb
+++ app/controllers/web/account/teams_controller.rb
@@ -11,9 +11,9 @@ class Web::Account::TeamsController < Web::Account::ApplicationController
     @team = TeamMutator.create(team_params, current_user)
 
     if @team.persisted?
-      redirect_to account_teams_path
+      redirect_to account_teams_path, notice: "Team is created"
     else
-      render :new
+      render :new, notice: "Something went wrong"
     end
   end
 
